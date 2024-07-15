dbfc9ee91e320d1eed28569b12e702f4798631cb
silvaPool <silva.friends22@gmail.com>
2024-07-15 13:08:21 -0300

    Organização no arquivo App.css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,31 +4,16 @@ import { Link } from "react-router-dom";
 
 function Layout() {
     return (
-        // <div id="grid">
-        //     <div id="left"></div>
-        //     <div id="right">
-
-        //       <Link to={'/opcoes'} className="link"> The BEATLES</Link>
-
-        //     </div>
-        // </div>
-
+      
         <Box className="main game">
             <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 5, border: 'none', height: '70vh', width: '135vh', padding: '10px', background: 'white'}}>
               <Box>
-                <Typography sx={{fontSize: '65px', fontFamily: " Playwrite GB J"}}>
+                <Typography sx={{fontSize: '65px', fontFamily: " Playwrite GB J", position: 'relative', bottom: '50px'}}>
                     The Beatles
                 </Typography>
                 <Link to={'/opcoes'} className="linkLayout">Explorar</Link>
                 </Box>
                 
-              {/* <div class="imgLayout">
-
-              </div> */}
-
-
-              {/* */}
-
            <div class="i-frame">
            <iframe
              width="560"
@@ -46,12 +31,6 @@ function Layout() {
 
             </Box>
         </Box>
-
-        // <iframe width="1000" height="315" src="https://www.youtube.com/embed/CGj85pVzRJs?si=W3maHJ6p0omnktaj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-
-        // <div class="hero game">
-        //      <Link to={'/opcoes'} className="link"> The BEATLES</Link>
-        // </div>
     )
 };
 
